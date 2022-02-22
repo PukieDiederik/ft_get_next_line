d5b1e11fbafbcbdf429e96e4220abf35cb883db7
Diederik Robert marie boeren <drobert-@c1r18s1.42lisboa.com>
2022-02-22 16:01:58 +0000

    fixed header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: drobert- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 10:50:18 by drobert-          #+#    #+#             */
-/*   Updated: 2022/02/22 15:37:39 by drobert-         ###   ########.fr       */
+/*   Updated: 2022/02/22 15:56:28 by drobert-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,6 @@ typedef struct s_buff
 }	t_buffer;
 
 //functions
+char	*get_next_line(int fd);
 
 #endif
